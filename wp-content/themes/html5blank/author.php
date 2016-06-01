5d34cb933c7d9856625a6fb6a5cf72bd9fe29888
Ela Yudhanira <elayudhanira@gmail.com>
2016-06-01 14:45:04 +0700

    Styling single posts

--- wp-content/themes/html5blank/author.php
+++ wp-content/themes/html5blank/author.php
@@ -6,7 +6,7 @@
 
 		<?php if (have_posts()): the_post(); ?>
 
-			<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+			<h2 class="subtitle"><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h2>
 
 		<?php if ( get_the_author_meta('description')) : ?>
 
@@ -24,30 +24,32 @@
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+					<a href="<?php the_permalink(); ?>" class="featured-image" title="<?php the_title(); ?>" style="background-image:url(<?php echo $image[0];?>);">
+
 					</a>
 				<?php endif; ?>
 				<!-- /post thumbnail -->
+				<div class="post-wrapper">
+					<!-- post title -->
+					<h2>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<!-- /Post title -->
 
-				<!-- post title -->
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<!-- /Post title -->
+					<!-- post details -->
+					<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+					<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+					<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+					<!-- /post details -->
 
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
+					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+					<br class="clear">
 
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
+					<?php edit_post_link(); ?>
+				</div>
 
 			</article>
 			<!-- /article -->
